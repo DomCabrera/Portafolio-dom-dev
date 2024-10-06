9ad7a7837a8dc3c59cdae78368f4c82493909252
DomCabrera <ahamkara35teo85@gmail.com>
2024-10-05 21:44:07 -0600

    First commit

---
+++ src/utils/tags.ts
@@ -0,0 +1,21 @@
+import ReactJS from "../Icons/ReactJS.astro";
+import TailwindCSS from "../Icons/TailwindCSS.astro";
+import Astro from "../Icons/AstroIcon.astro";
+
+export const TAGS = {
+  REACT: {
+    name: "React",
+    class: "bg-[#23272F] text-white",
+    icon: ReactJS,
+  },
+  TAILWIND: {
+    name: "Tailwind CSS",
+    class: "bg-[#003159] text-white",
+    icon: TailwindCSS,
+  },
+  ASTRO: {
+    name: "Astro",
+    class: "bg-black text-white",
+    icon: Astro,
+  },
+};
